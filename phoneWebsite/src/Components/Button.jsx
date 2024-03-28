81d36a9e2954c1593afe4f0c865cb7a2fa8df3b8
Dido2002 <didoguzelev@gmail.com>
2024-03-28 09:04:07 +0200

    CreatedComponendForRenderingBtn|Link

--- phoneWebsite/src/Components/Button.jsx
+++ phoneWebsite/src/Components/Button.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
-const Button = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
+const Button = ({className, href, onClick, children, px}) => {
+
+    const classes = `button relative inline-flex items-center justify-center h-11 transition-color hover:text-color-1 ${
+        px || "px-7"
+      }`;
+    
+    const spanClasses = 'relative z-10'
+    const renderButton = () => (
+        <button onClick={onClick} className={classes}>
+            <span className={spanClasses}>{children}</span>
+        </button>
+    );
+    const renderLink = () => (
+        <a href={href} className={classes}>
+            <span className={spanClasses}>{children}</span>
+        </a>
+    );
+    return href ? renderLink() : renderButton();
+ 
+};
 
 export default Button
