4f9c1042d78ca59d6a3cacda7c66ec815b8166de
Dido2002 <didoguzelev@gmail.com>
2024-03-29 10:04:22 +0200

    TinyChanges&Fixes

--- phoneWebsite/src/Components/Navbar.jsx
+++ phoneWebsite/src/Components/Navbar.jsx
@@ -1,55 +1,48 @@
-import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
-import { Navigation } from "../Constants/Navigation";
-import MenuSvg from "../assets/Menu";
+import React,{useState} from 'react'
+import MenuSvg from '../assets/Menu'
+import {Navigation} from "../Constants/Navigation"
 import Button from "./Button";
+
 const Navbar = () => {
+  
   const [openNavigation, setOpenNavigation] = useState(false);
 
   const toogleNavigation = () => {
     if(openNavigation){
-      setOpenNavigation(false);
+      setOpenNavigation(false)
     }
     else{
-      setOpenNavigation(true);
+      setOpenNavigation(true)
     }
   }
-  const handleClick = () => {
-    if(!openNavigation) return;
-    setOpenNavigation(false);
-  }
-
+  
   return (
-    <div className="w-full h-[6rem] pt-4 flex justify-between px-[8rem] bg-none items-center fixed z-2">
-      <nav>
-        <ul className={`${openNavigation ? "block" : "hidden"} h-auto w-[22rem] flex justify-between items-center`}>
-          {Navigation.map((item) => (
-            <li
-              onClick={handleClick}
-              key={item.id}
-              className="text-white text-lg font-normal tracking-wide"
-            >
-              <a href={item.url}>{item.title}</a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <div className="logo h-auto w-[20rem] flex justify-start items-center">
-        <h2 className="text-white text-4xl mb-3 traking-wider font-bold">
-          smartHome
-        </h2>
-      </div>
-      <div className="flex w-[15rem] justify-between items-center">
-        <button className="bg-none text-lg text-white">Login</button>
-        <Button>
-          <span className="text-lg">Sign Up</span>
-        </Button>
-        <button className="ml-auto lg:hidden" onClick={toogleNavigation}>
+    <div className="w-full h-full lg:h-[6rem] flex justify-center items-start lg:items-center z-1 fixed bg-transparent">
+      <nav className="w-full h-auto px-[4rem] flex justify-around items-center pt-4">
+        <div className='nav-links w-[24rem] h-full flex justify-center items-center'>
+          <ul className={`${openNavigation ? "flex flex-col" : "hidden"} lg:flex w-full h-auto items-center justify-around`}>
+            {Navigation.map((item) => (
+              <a className='text-white' href={item.url} key={item.id}>{item.title}</a>
+            ))}
+            <div className={`${openNavigation ? "flex flex-col items-center justify-center" : "hidden"}`}>
+              <a href="#">Login</a>
+              <a href="#">Sign Up</a>
+            </div>
+          </ul>
+        </div>
+        <div className='logo flex justify-center items-center'>
+          <h2 className='text-white text-3xl font-bold mb-2'>smartHome</h2>
+        </div>
+        <div className='btn-container hidden lg:flex'>
+          <button className='text-white'>Login</button>
+          <Button>Sign Up</Button>
+        </div>
+        <div onClick={toogleNavigation} className='menu lg:hidden flex'>
           <MenuSvg openNavigation={openNavigation}/>
-        </button>
-      </div>
+        </div>
+      </nav>
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
