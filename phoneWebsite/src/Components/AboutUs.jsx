855d577aa24c893ff24f308a61492b9d99280a28
Dido2002 <didoguzelev@gmail.com>
2024-04-03 15:08:31 +0300

    AboutUsStarted

--- phoneWebsite/src/Components/AboutUs.jsx
+++ phoneWebsite/src/Components/AboutUs.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import google_play from "../assets/Images/google-play.png"
+import apple_store from "../assets/Images/apple_store.png"
 
 const AboutUs = () => {
   return (
-    <div className='w-full h-auto'>
-        AboutUs
+    <div className='w-full h-auto pt-[24rem]'>
+        <div className='btn-container w-full h-auto flex justify-center items-center gap-16'>
+          <div className='apple-store w-[14rem] bg-black h-auto py-2 rounded-2xl border flex justify-center items-center'>
+            <div className='icon-btn'>
+              <img src={apple_store} width={60} alt="" />
+            </div>
+            <div className='text-btn text-white'>
+              <p className='font-semibold'>Download on the</p>
+              <p className='text-3xl font-semibold'>App Store</p>
+            </div>
+          </div>
+          <div className='google-play w-[14rem] bg-black h-auto p-2 rounded-2xl border flex justify-center items-center'>
+            <div className='icon-btn'>
+              <img src={google_play} width={40} alt="" />
+            </div>
+            <div className='text-btn text-white'>
+              <p className='font-semibold'>Get it on</p>
+              <p className='text-3xl font-semibold tracking-tight'>Google Play</p>
+            </div>
+          </div>
+        </div>
+        <div className='title-about-us w-full h-auto flex justify-center items-center mt-[10rem]'>
+          <h1 className='text-8xl font-black'>About Us</h1>
+        </div>
     </div>
   )
 }
