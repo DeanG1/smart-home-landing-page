b37272df6abde995354b816219e848f41f0d91a2
Dido2002 <didoguzelev@gmail.com>
2024-04-05 17:13:16 +0300

    FinalChangesForTodayClientSectionAllmostReady

--- phoneWebsite/src/Constants/ClientsInfo.js
+++ phoneWebsite/src/Constants/ClientsInfo.js
@@ -1,19 +1,60 @@
-import travel from "../assets/Images/travel.png"; 
+import travel from "../assets/Images/travel.png";
 import octane from "../assets/Images/octane.png";
+import lifefitness from "../assets/Images/lifefitness.png";
+import smile from "../assets/Images/smile.png";
+import trueClient from "../assets/Images/true.png";
+import cybex from "../assets/Images/cybex.png";
+import mockup from "../assets/Images/mockup.png";
+import precor from "../assets/Images/precor.png";
 
 const ClientsInfo = [
-    {
-        id:'0',
-        name:"Travel",
-        alt:"Travel",
-        image:travel
-    },
-    {
-        id:'1',
-        name:"Octane",
-        alt:"Octane",
-        image:octane
-    }
-
+  {
+    id: "0",
+    name: "Travel",
+    alt: "Travel",
+    image: travel,
+  },
+  {
+    id: "1",
+    name: "Octane",
+    alt: "Octane",
+    image: octane,
+  },
+  {
+    id: "2",
+    name: "LifeFitness",
+    alt: "LifeFitness",
+    image: lifefitness,
+  },
+  {
+    id: "3",
+    name: "Smile",
+    alt: "Smile",
+    image: smile,
+  },
+  {
+    id: "4",
+    name: "True",
+    alt: "True",
+    image: trueClient,
+  },
+  {
+    id: "5",
+    name: "Cybex",
+    alt: "Cybex",
+    image: cybex,
+  },
+  {
+    id: "6",
+    name: "Mockup",
+    alt: "Mockup",
+    image: mockup,
+  },
+  {
+    id: "7",
+    name: "Precor",
+    alt: "Precor",
+    image: precor,
+  },
 ];
-export default ClientsInfo; 
+export default ClientsInfo;
