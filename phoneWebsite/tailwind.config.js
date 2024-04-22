e5bb754eb000577a743db91191e0043b8357f0f0
Dido2002 <didoguzelev@gmail.com>
2024-04-22 22:21:43 +0300

    TestimonialCreatedAndReadyNotAdjustedToBeResponsive

--- phoneWebsite/tailwind.config.js
+++ phoneWebsite/tailwind.config.js
@@ -1,37 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       boxShadow: {
-        '3xl': '0 0px 20px 5px rgba(0, 0, 0, 0.17)',
-        '4xl': '0px 40px 35px 6px rgba(0, 0, 0, 0.1)'
+        "3xl": "0 0px 20px 5px rgba(0, 0, 0, 0.17)",
+        "4xl": "0px 40px 35px 6px rgba(0, 0, 0, 0.1)",
       },
-      borderColor:{
-        'primary': "#333333"
+      borderColor: {
+        primary: "#333333",
       },
-      backgroundColor:{
-        primary:"#1E5AFA",
-        secondary:"#333333",
-        btn_background:"#000F37"
+      backgroundColor: {
+        primary: "#1E5AFA",
+        secondary: "#333333",
+        btn_background: "#000F37",
       },
-      textColor:{
-        primary:"#575757",
-        secondary:"#313131"
+      textColor: {
+        primary: "#575757",
+        secondary: "#313131",
+        light_grey: "#888",
       },
-      fontSize:{
-        'xl7-1/2': '5.8rem'
+      fontSize: {
+        "xl7-1/2": "5.8rem",
       },
-      fontFamily:{
-        'sora': ['Sora', 'sans-serif'],
-        'code': ['Source Code Pro', 'monospace'],
-        'grotesk': ['Space Grotesk', 'sans-serif'],
-      }
+      fontFamily: {
+        sora: ["Sora", "sans-serif"],
+        code: ["Source Code Pro", "monospace"],
+        grotesk: ["Space Grotesk", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
-
+};
